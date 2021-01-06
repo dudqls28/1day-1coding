aa12edb625591d6c7f90c2f84635642b5c9f1773
dudqls28 <68379750+dudqls28@users.noreply.github.com>
2021-01-06 21:35:15 +0900

    FailureRate Finish

--- src/Programmer/Level1/FailureRate/FailureRate.java
+++ src/Programmer/Level1/FailureRate/FailureRate.java
@@ -3,42 +3,59 @@ package Programmer.Level1.FailureRate;
 import java.util.ArrayList;
 import java.util.HashSet;
 
+
+
 class Solution {
-
-	public int[] solution(int[] array, int[][] commands) {
-        int[] answer = {};
-        answer = new int[commands.length];
-        int i,j,k;
-        HashSet<Integer> set=new HashSet<Integer>();
-
-        for(int x=0;x<commands.length;x++){
-        i=commands[x][0];
-        j=commands[x][1];
-        k=commands[x][2];
-        for(int q=i-1;q<j;){
-          set.add(array[q]);
-            q++;
+    public int[] solution(int N, int[] stages) {
+        int[] answer = new int[N];
+        double[] tempArr = new double[N];
+        int arrLength = stages.length;
+        int idx = arrLength;
+        double tempD = 0;
+        int tempI = 0;
+        for (int i = 0; i < arrLength; i++) {
+            int stage = stages[i];
+            if (stage != N + 1)
+                answer[stage - 1] += 1;
+           
         }
-        System.out.println("");
-        ArrayList<Integer> list=new ArrayList<Integer>(set);
-        answer[x]=list.get(k-1);
-    }
+        
+        for (int i = 0; i < N; i++) {
+            int personNum = answer[i];
+            tempArr[i] = (double) personNum / idx;
+            idx -= personNum;
+            answer[i] = i + 1;
+        }
+
+        for (int i = 0; i < N; i++) {
+            for (int j = 1; j < N - i; j++) {
+                if (tempArr[j - 1] < tempArr[j]) {
+                    tempD = tempArr[j - 1];
+                    tempArr[j - 1] = tempArr[j];
+                    tempArr[j] = tempD;
+
+                    tempI = answer[j - 1];
+                    answer[j - 1] = answer[j];
+                    answer[j] = tempI;
+                }
+            }
+        }
         return answer;
-}
+    }
 }
 
 public class FailureRate{
+
+public static void main(String[] args) {
+	int n= 5;
+	int[] stages= {2, 1, 2, 6, 2, 4, 3, 3};
 	
-	public static void main(String[] args) {
-		int[] array= {1, 5, 2, 6, 3, 7, 4};
-		int[][] commands= {{2, 5, 3}, {4, 4, 1}, {1, 7, 3}};
-		
-		Solution s = new Solution();
-		
-		int[] answer= s.solution(array, commands);
-		for(int i=0;i<answer.length;i++) {
-		System.out.println(answer[i]);
-		}
+	Solution s = new Solution();
 	
+	int[] answer= s.solution(n, stages);
+	for(int i=0;i<answer.length;i++) {
+	System.out.print(answer[i]+",");
+	}
+
 }
 }
