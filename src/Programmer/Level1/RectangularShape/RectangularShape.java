90348059f829fd93d675f0cc8b57b2bdea5e032e
dudqls28 <68379750+dudqls28@users.noreply.github.com>
2021-01-11 16:23:01 +0900

    2021-01-11 RectangularShape Final

--- src/Programmer/Level1/RectangularShape/RectangularShape.java
+++ src/Programmer/Level1/RectangularShape/RectangularShape.java
@@ -6,10 +6,7 @@ import java.util.*;
 public class RectangularShape{
 
 public static void main(String[] args) {
-	
 
-	
-	  
 	        Scanner sc = new Scanner(System.in);
 	        int a = sc.nextInt();
 	        int b = sc.nextInt();
