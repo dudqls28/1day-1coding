720582d6cadc2b5e2dd4f5f19c480a5513dd84d1
dudqls28 <68379750+dudqls28@users.noreply.github.com>
2021-02-13 13:56:24 +0900

    2021.02.13 Keypad Clear!!

--- src/Programmer/Level1/Keypad/KeyPad.java
+++ src/Programmer/Level1/Keypad/KeyPad.java
@@ -3,38 +3,97 @@ package Programmer.Level1.Keypad;
 import java.util.ArrayList;
 import java.util.HashSet;
 
-//String binaryString = Integer.toBinaryString(i); //2進数
-//String octalString = Integer.toOctalString(i);   //8進数
-//String hexString = Integer.toHexString(i);      //16進数
 
 class Solution {
-    public String[] solution(int n, int[] arr1, int[] arr2) {
-        String[] answer = new String[n];
-        
-        for( int i=0;i<n;i++){
-        	// Integer.parseInt 使用したか範囲超過 Long.ParseLongに代替
-        	// format 使用して0のせきお作った
-            answer[i]=String.format("%0"+n+"d", Long.parseLong(Integer.toBinaryString(arr1[i]|arr2[i])));
-            
-            answer[i]=answer[i].replace("1","#");
-            answer[i]=answer[i].replace("0"," ");
-        }
-        return answer;
-}
+	 public String solution(int[] numbers, String hand) {
+	        String answer = "";
+	        int[] L_position = {4,1};
+	        int[] R_position = {4,3};
+	        int[] temp = {0,0};
+	        for(int i=0 ; i<numbers.length;i++){
+	            int L_stack=0;
+	            int R_stack=0;
+	            switch(numbers[i]){
+	                case 1 : answer+="L";
+	                         L_position[0]=1;L_position[1]=1;
+	                    break;
+	                case 2 : temp[0]=1; temp[1]=2;
+	                    break;
+	                case 3 : answer+="R";
+	                         R_position[0]=1;R_position[1]=3;
+	                    break;
+	                case 4 : answer+="L";
+	                         L_position[0]=2;L_position[1]=1;
+	                    break;
+	                case 5 : temp[0]=2; temp[1]=2;
+	                    break;
+	                case 6 : answer+="R";
+	                         R_position[0]=2;R_position[1]=3;
+	                    break;
+	                case 7 : answer+="L";
+	                         L_position[0]=3;L_position[1]=1;
+	                    break;
+	                case 8 : temp[0]=3; temp[1]=2;
+	                    break;
+	                case 9 : answer+="R";
+	                         R_position[0]=3;R_position[1]=3;
+	                    break;
+	                case 0 : temp[0]=4; temp[1]=2;
+	                    break;
+	            }
+	            System.out.println("왼손위치 : " + L_position[0]+","+L_position[1]);
+	                System.out.println("오른손위치 : " + R_position[0]+","+R_position[1]);
+	            if(numbers[i]==2 ||numbers[i]==5 ||numbers[i]==8 ||numbers[i]==0){
+	            for(int j=0;j<2;j++){
+	                if(L_position[j]>temp[j]){
+	                    L_stack+=L_position[j]-temp[j];
+	                }else{
+	                    L_stack+=temp[j]-L_position[j];
+	                }
+	               if(R_position[j]>temp[j]){
+	                    R_stack+=R_position[j]-temp[j];
+	                }else{
+	                    R_stack+=temp[j]-R_position[j];
+	                }
+	            }
+	                System.out.println("왼손스택 : " + L_stack);
+	                System.out.println("오른손스택 : " + R_stack);
+	                
+	            if(L_stack>R_stack){
+	                    answer+="R";
+	                    R_position[0]=temp[0];
+	                    R_position[1]=temp[1];
+	                }else if(R_stack>L_stack){
+	                    answer+="L";
+	                    L_position[0]=temp[0];
+	                    L_position[1]=temp[1];
+	                }else if(R_stack==L_stack && hand.equals("right")){
+	                    answer+="R";
+	                    System.out.println("라이트");
+	                    R_position[0]=temp[0];
+	                    R_position[1]=temp[1];
+	                }else if(R_stack==L_stack && hand.equals("left")){
+	                    answer+="L";
+	                System.out.println("레프트");
+	                    L_position[0]=temp[0];
+	                    L_position[1]=temp[1];
+	                }
+	        }
+	        }
+	        return answer;
+	    }
 }
 public class KeyPad{
 
 public static void main(String[] args) {
-	int n= 5;
-	int[] arr1= {9, 20, 28, 18, 11};
-	int[] arr2= {30, 1, 21, 17, 28};
+	int[] numbers= {1, 3, 4, 5, 8, 2, 1, 4, 5, 9, 5};
+	String hand ="right";
+	//answer = "LRLLLRLLRRL"
 	
 	Solution s = new Solution();
 	
-	String[] answer= s.solution(n,arr1,arr2);
-	for(int i=0;i<answer.length;i++) {
-	System.out.print(answer[i]+",");
-	}
+	String answer= s.solution(numbers,hand);
+	System.out.println(answer);
 
 }
 }
