365a3988a79612b0b8d1fe51864033f5d024455b
dudqls28 <68379750+dudqls28@users.noreply.github.com>
2021-03-16 21:19:11 +0900

    2021.03.16 Pokemon Clear!!

--- src/Programmer/Level1/Pokemon/Pokemon.java
+++ src/Programmer/Level1/Pokemon/Pokemon.java
@@ -3,32 +3,22 @@ package Programmer.Level1.Pokemon;
 import java.util.*;
 
 class Solution {
-	public int solution(int[] nums) {
-        int answer = 0;
-        int choice = nums.length/2;
-        HashSet<Integer> set=new HashSet<Integer>();
-        for(int i = 0 ;i<nums.length;i++){
-            set.add(nums[i]);
-        }
-        ArrayList<Integer> list= new ArrayList<Integer>(set);
-        Iterator it =list.iterator();
-        int[] poketmon = new int[list.size()];
-        int count=0;
-        while(it.hasNext()) {
-        	poketmon[count]=(int)it.next();
-        	System.out.println(poketmon[count]);
-        	count++;
-        }
-        for(int i =0 ;i<poketmon.length;i++) {
-        	for(int j = i+1 ;j<poketmon.length;j++) {
-        		answer++;
-        	}
-        }
-        if(answer<choice){
-            answer=choice;
-        }
-    return answer;
-}
+    public int solution(int[] nums) {
+            int answer = 0;
+            int choice = nums.length/2;
+	        HashSet<Integer> set=new HashSet<Integer>();
+	        for(int i = 0 ;i<nums.length;i++){
+	            set.add(nums[i]);
+	        }
+	        ArrayList<Integer> list= new ArrayList<Integer>(set);
+	        answer=list.size();
+            System.out.println(answer);
+            if(answer>choice){
+                answer=choice;
+            }
+        return answer;
+    }
+
 }
 
 public class Pokemon{
